a029c534136c86328067a450c61be3f56fcd548f
Dragan Cabarkapa <caboocg@gmail.com>
2016-11-06 02:03:17 +0100

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :webhooks, only: [:create]
+  post '/webhooks/:id', to: 'webhooks#create'
 
   root to: 'status#status'
 end
